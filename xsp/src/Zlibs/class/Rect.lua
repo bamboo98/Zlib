be7b7a8990c836c85d2bc1f20d99a7950619800c
BAMBOO <454689880@qq.com>
2019-07-05 21:17:07 +0800

    update

--- xsp/src/Zlibs/class/Rect.lua
+++ xsp/src/Zlibs/class/Rect.lua
@@ -1,271 +1,249 @@
 local Zlog = require "Zlibs.class.Log"
 local Point = require "Zlibs.class.Point"
 local Size = require "Zlibs.class.Size"
---local Circle = require "Zlibs.class.Circle"
+-- local Circle = require "Zlibs.class.Circle"
 local math = require "Zlibs.class.math"
 local type = require "Zlibs.tool.type"
 local string = require "Zlibs.class.string"
-local obj={}
-local funcValues={}
+local obj = {}
+local funcValues = {}
 local AllRect = {}
-local locationMode=-1
-local naiveRect=rawget(_G,"Rect")
+local locationMode = -1
+local naiveRect = rawget(_G, "Rect")
 local api
---默认变量
-obj.__tag="Rect"
-obj.name="default"
-obj.x=0
-obj.y=0
-obj.width=0
-obj.height=0
-obj.distroyoyed=false
---默认变量结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---元表设置
-function obj:__newindex(k,v)
-	rawset(self,k,v)
-	Zlog.warn("不建议的操作:设置了[%s]对象中[%s]元素的值为[%s]",self.__tag,tostring(k),tostring(v))
+-- 默认变量
+obj.__tag = "Rect"
+obj.name = "default"
+obj.x = 0
+obj.y = 0
+obj.width = 0
+obj.height = 0
+obj.distroyoyed = false
+-- 默认变量结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 元表设置
+function obj:__newindex(k, v)
+    rawset(self, k, v)
+    Zlog.warn(
+        "不建议的操作:设置了[%s]对象中[%s]元素的值为[%s]",
+        self.__tag, tostring(k), tostring(v))
 end
 function obj:__index(k)
-	if type(funcValues[k])=="function" then return funcValues[k](self) end
-	return obj[k]
+    if type(funcValues[k]) == "function" then return funcValues[k](self) end
+    return obj[k]
 end
 function obj:__tostring()
-	if self.name=="default" then
-		return string.format("%s(%d ,%d ,%d ,%d)",obj.__tag,self.x,self.y,self.width,self.height)
-	elseif locationMode==1 then
-		return string.format("%s \"%s\" \"%d,%d,%d,%d\"",obj.__tag,
-			self.name,
-			self.x,
-			self.y,
-			self.x+self.width,
-			self.y+self.height
-		)
-	elseif locationMode==2 then
-		return string.format("%s \"%s\" \"%d,%d,%d,%d\"",obj.__tag,
-			self.name,
-			self.x,
-			self.y,
-			self.width,
-			self.height
-		)
-	end
+    if self.name == "default" then
+        return string.format("%s(%d ,%d ,%d ,%d)", obj.__tag, self.x, self.y,
+                             self.width, self.height)
+    elseif locationMode == 1 then
+        return string.format("%s \"%s\" \"%d,%d,%d,%d\"", obj.__tag, self.name,
+                             self.x, self.y, self.x + self.width,
+                             self.y + self.height)
+    elseif locationMode == 2 then
+        return string.format("%s \"%s\" \"%d,%d,%d,%d\"", obj.__tag, self.name,
+                             self.x, self.y, self.width, self.height)
+    end
 end
---并集
+-- 并集
 function obj:__add(r)
-	local r1tl,r1br=self:tl(),self:br()
-	local r2tl,r2br=r:tl(),r:br()
-	return obj:__call(math.min(r1tl.x,r2tl.x),math.min(r1tl.y,r2tl.y),math.max(r1br.x,r2br.x)-math.min(r1tl.x,r2tl.x),math.max(r1br.y,r2br.y)-math.min(r1tl.y,r2tl.y))
+    local r1tl, r1br = self:tl(), self:br()
+    local r2tl, r2br = r:tl(), r:br()
+    return obj:__call(math.min(r1tl.x, r2tl.x), math.min(r1tl.y, r2tl.y),
+                      math.max(r1br.x, r2br.x) - math.min(r1tl.x, r2tl.x),
+                      math.max(r1br.y, r2br.y) - math.min(r1tl.y, r2tl.y))
 end
---交集
+-- 交集
 function obj:__sub(r)
-	local r1tl,r1br=self:tl(),self:br()
-	local r2tl,r2br=r:tl(),r:br()
-	--矩形不相交时返回ZERO
-	if not self:isintersect(r) then
-		return obj.ZERO
-	end
-	return obj:__call(math.max(r1tl.x,r2tl.x),math.max(r1tl.y,r2tl.y),math.min(r1br.x,r2br.x)-math.max(r1tl.x,r2tl.x),math.min(r1br.y,r2br.y)-math.max(r1tl.y,r2tl.y))
+    local r1tl, r1br = self:tl(), self:br()
+    local r2tl, r2br = r:tl(), r:br()
+    -- 矩形不相交时返回ZERO
+    if not self:isintersect(r) then return obj.ZERO end
+    return obj:__call(math.max(r1tl.x, r2tl.x), math.max(r1tl.y, r2tl.y),
+                      math.min(r1br.x, r2br.x) - math.max(r1tl.x, r2tl.x),
+                      math.min(r1br.y, r2br.y) - math.max(r1tl.y, r2tl.y))
 end
---缩放
+-- 缩放
 function obj:__mul(n)
-	local c=self:center()
-	local s=self:size()
-	local as=s*n
-	return obj:__call(math.round(c.x-as.width/2),math.round(c.y-as.height/2),as.width,as.height)
+    local c = self:center()
+    local s = self:size()
+    local as = s * n
+    return obj:__call(math.round(c.x - as.width / 2),
+                      math.round(c.y - as.height / 2), as.width, as.height)
 end
---属性叠加
+-- 属性叠加
 function obj:__div(r)
-	return obj:__call(self.x+r.x,self.y+r.y,self.width+r.width,self.height+r.height)
+    return obj:__call(self.x + r.x, self.y + r.y, self.width + r.width,
+                      self.height + r.height)
 end
---相等
+-- 相等
 function obj:__eq(s)
-	return self.width==s.width and self.height==s.height and self.x==s.x and self.y==s.y
+    return
+        self.width == s.width and self.height == s.height and self.x == s.x and
+            self.y == s.y
 end
 function obj:__call(...)
-	local o={
-		x=0,
-		y=0,
-		width=0,
-		height=0
-	}
-	setmetatable(o, obj)
-	local t={...}
-	if #t==1 and type(t[1])=="string" then
-		if AllRect[t[1]] then
-			Zlog.warn("点类型出现重名[%s],旧数据将会被新数据覆盖",t[1])
-			o=AllRect[t[1]]
-		end
-		rawset(o,"name",t[1])
-		AllRect[o.name]=o
-		return function(data)
-			if type(data)=="string" then
-				data=string.split(data,",")
-			end
-			--这里用table.unpack是否为nil来判断是1.9引擎还是2.0引擎(lua5.3的独有函数)
-			if locationMode==2 then
-				o.x=data[1]
-				o.y=data[2]
-				o.width=data[3]
-				o.height=data[4]
-			elseif locationMode==1 then
-				o.x=data[1]
-				o.y=data[2]
-				o.width=data[3]-data[1]
-				o.height=data[4]-data[2]
-			else
-				Zlog.fatal "使用此方法创建Rect前请先设置好坐标类型\r设置代码:  require \"Zlibs.class.Rect\".setLocationMode(1或2)\r1代表采用1.9的坐标,后两位表示右下角点的坐标,即x1,y1,x2,y2\r2代表采用2.0的坐标,后两位表示矩形的大小,即x1,y1,width,height"
-			end
-			return o
-		end
-	else
-		local x,y,w,h
-		if #t==0 then
-			x,y,w,h=0,0,0,0
-		elseif #t==1 and type(t[1])=="Rect" then
-			x,y,w,h=t[1].x,t[1].y,t[1].width,t[1].height
-		elseif #t==1 and type(t[1])=="Size" then
-			x,y,w,h=0,0,t[1].width,t[1].height
-		elseif #t==1 and type(t[1])=="Point" then
-			x,y,w,h=t[1].x,t[1].y,0,0
-		elseif #t==2 and type(t[1])=="Point" and type(t[2])=="Size" then
-			x,y,w,h=t[1].x,t[1].y,t[2].width,t[2].height
-		elseif #t==4 then
-			x,y,w,h=...
-		else
-			Zlog.fatal("[%s]创建时参数传入错误",self.__tag)
-		end
-		o.x,o.y,o.width,o.height=math.round(x),math.round(y),math.round(math.max(0,w)),math.round(math.max(0,h))
-		return o
-	end
+    local o = {x = 0, y = 0, width = 0, height = 0}
+    setmetatable(o, obj)
+    local t = {...}
+    if #t == 1 and type(t[1]) == "string" then
+        if AllRect[t[1]] then
+            Zlog.warn(
+                "点类型出现重名[%s],旧数据将会被新数据覆盖",
+                t[1])
+            o = AllRect[t[1]]
+        end
+        rawset(o, "name", t[1])
+        AllRect[o.name] = o
+        return function(data)
+            if type(data) == "string" then
+                data = string.split(data, ",")
+            end
+            -- 这里用table.unpack是否为nil来判断是1.9引擎还是2.0引擎(lua5.3的独有函数)
+            if locationMode == 2 then
+                o.x = data[1]
+                o.y = data[2]
+                o.width = data[3]
+                o.height = data[4]
+            elseif locationMode == 1 then
+                o.x = data[1]
+                o.y = data[2]
+                o.width = data[3] - data[1]
+                o.height = data[4] - data[2]
+            else
+                Zlog.fatal "使用此方法创建Rect前请先设置好坐标类型\r设置代码:  require \"Zlibs.class.Rect\".setLocationMode(1或2)\r1代表采用1.9的坐标,后两位表示右下角点的坐标,即x1,y1,x2,y2\r2代表采用2.0的坐标,后两位表示矩形的大小,即x1,y1,width,height"
+            end
+            return o
+        end
+    else
+        local x, y, w, h
+        if #t == 0 then
+            x, y, w, h = 0, 0, 0, 0
+        elseif #t == 1 and type(t[1]) == "Rect" then
+            x, y, w, h = t[1].x, t[1].y, t[1].width, t[1].height
+        elseif #t == 1 and type(t[1]) == "Size" then
+            x, y, w, h = 0, 0, t[1].width, t[1].height
+        elseif #t == 1 and type(t[1]) == "Point" then
+            x, y, w, h = t[1].x, t[1].y, 0, 0
+        elseif #t == 2 and type(t[1]) == "Point" and type(t[2]) == "Size" then
+            x, y, w, h = t[1].x, t[1].y, t[2].width, t[2].height
+        elseif #t == 2 and type(t[1]) == "Point" and type(t[2]) == "Point" then
+            x, y, w, h = math.min(t[1].x, t[2].x), math.min(t[1].y, t[2].y),
+                         math.abs(t[2].x - t[1].x), math.abs(t[2].y - t[1].y)
+        elseif #t == 4 then
+            x, y, w, h = ...
+        else
+            Zlog.fatal("[%s]创建时参数传入错误", self.__tag)
+        end
+        o.x, o.y, o.width, o.height = math.round(x), math.round(y),
+                                      math.round(math.max(0, w)),
+                                      math.round(math.max(0, h))
+        return o
+    end
 end
---元表设置结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---自动变量
+-- 元表设置结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 自动变量
 function funcValues.randomPoint(self)
-	return Point(math.random(self.x,self.x+self.width),math.random(self.y,self.y+self.height))
+    return Point(math.random(self.x, self.x + self.width),
+                 math.random(self.y, self.y + self.height))
 end
 
---自动变量结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---内部函数
+-- 自动变量结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 内部函数
 
---内部函数结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---成员函数
-function obj.get(name)
-	return AllRect[name]
-end
+-- 内部函数结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 成员函数
+function obj.get(name) return AllRect[name] end
 function obj.distroy(self)
-	if type(self)=="string" then
-		self=obj.get(self)
-	end
-	AllRect[self.name]=nil
-	rawset(self,"distroyoyed",true)
+    if type(self) == "string" then self = obj.get(self) end
+    AllRect[self.name] = nil
+    rawset(self, "distroyoyed", true)
 end
---获取左上角
-function obj.tl(self)
-	return Point(self.x,self.y)
+-- 获取左上角
+function obj.tl(self) return Point(self.x, self.y) end
+-- 获取右下角
+function obj.br(self) return Point(self.x + self.width, self.y + self.height) end
+-- 获取大小
+function obj.size(self) return Size(self.width, self.height) end
+-- 是否包含点
+function obj.contains(self, p)
+    if type(p) == "Point" then
+        return
+            self.x <= p.x and self.y <= p.y and self.x + self.width >= p.x and
+                self.y + self.height >= p.y
+    elseif type(p) == "Rect" then
+        return self.x <= p.x and self.y <= p.y and self.x + self.width >= p.x +
+                   p.width and self.y + self.height >= p.y + p.height
+    elseif type(p) == "Circle" then
+        local c = p:center()
+        return c.x - self.x >= p.r and c.y - self.y >= p.r and self.x +
+                   self.width - c.x >= p.r and self.y + self.height - c.y >= p.r
+    end
 end
---获取右下角
-function obj.br(self)
-	return Point(self.x+self.width,self.y+self.height)
+-- 包含2个矩形的最小矩形
+function obj.union(self, r) if type(r) == "Rect" then return self + r end end
+-- 取交集
+function obj.intersect(self, r) if type(r) == "Rect" then return self - r end end
+-- 获取中心点
+function obj.center(r) return Point(r.x + r.width / 2, r.y + r.height / 2) end
+-- 矩形是否相交(边与边重合视为相交)
+function obj.isintersect(r1, r2)
+    return r1.x <= r2.x + r2.width and r1.y <= r2.y + r2.height and r1.x +
+               r1.width >= r2.x and r1.y + r1.height >= r2.y
 end
---获取大小
-function obj.size(self)
-	return Size(self.width,self.height)
-end
---是否包含点
-function obj.contains(self,p)
-	if type(p)=="Point" then
-		return self.x<=p.x and self.y<=p.y and self.x+self.width>=p.x and self.y+self.height>=p.y
-	elseif type(p)=="Rect" then
-		return self.x<=p.x and self.y<=p.y and self.x+self.width>=p.x+p.width and self.y+self.height>=p.y+p.height
-	elseif type(p)=="Circle" then
-		local c=p:center()
-		return c.x-self.x>=p.r and c.y-self.y>=p.r and self.x+self.width-c.x>=p.r and self.y+self.height-c.y>=p.r
-	end
-end
---包含2个矩形的最小矩形
-function obj.union(self,r)
-	if type(r)=="Rect" then
-		return self + r
-	end
-end
---取交集
-function obj.intersect(self,r)
-	if type(r)=="Rect" then
-		return self - r
-	end
-end
---获取中心点
-function obj.center(r)
-	return Point(r.x+r.width/2,r.y+r.height/2)
-end
---矩形是否相交(边与边重合视为相交)
-function obj.isintersect(r1,r2)
-	return r1.x<=r2.x+r2.width and r1.y<=r2.y+r2.height and r1.x+r1.width>=r2.x and r1.y+r1.height>=r2.y
-end
---获取外接圆
+-- 获取外接圆
 function obj.outCircle(self)
-	local Circle = require "Zlibs.class.Circle"
-	return Circle(self:center(),self:tl())
+    local Circle = require "Zlibs.class.Circle"
+    return Circle(self:center(), self:tl())
 end
---获取内接圆
+-- 获取内接圆
 function obj.inCircle(self)
-	local Circle = require "Zlibs.class.Circle"
-	return Circle(self:center(),math.min(self.height,self.width)/2)
+    local Circle = require "Zlibs.class.Circle"
+    return Circle(self:center(), math.min(self.height, self.width) / 2)
 end
-function obj.setLocationMode(mode)
-	locationMode=mode
-end
+function obj.setLocationMode(mode) locationMode = mode end
 function obj.toString(self)
-	if locationMode==1 then
-		return string.format("%d,%d,%d,%d",
-			self.x,
-			self.y,
-			self.x+self.width,
-			self.y+self.height
-		)
-	elseif locationMode==2 then
-		return string.format("%d,%d,%d,%d",
-			self.x,
-			self.y,
-			self.width,
-			self.height
-		)
-	else
-		Zlog.fatal "使用此方法创建Rect前请先设置好坐标类型\r设置代码:  require \"Zlibs.class.Rect\".setLocationMode(1或2)\r1代表采用1.9的坐标,后两位表示右下角点的坐标,即x1,y1,x2,y2\r2代表采用2.0的坐标,后两位表示矩形的大小,即x1,y1,width,height"
-	end
+    if locationMode == 1 then
+        return string.format("%d,%d,%d,%d", self.x, self.y, self.x + self.width,
+                             self.y + self.height)
+    elseif locationMode == 2 then
+        return string.format("%d,%d,%d,%d", self.x, self.y, self.width,
+                             self.height)
+    else
+        Zlog.fatal "使用此方法创建Rect前请先设置好坐标类型\r设置代码:  require \"Zlibs.class.Rect\".setLocationMode(1或2)\r1代表采用1.9的坐标,后两位表示右下角点的坐标,即x1,y1,x2,y2\r2代表采用2.0的坐标,后两位表示矩形的大小,即x1,y1,width,height"
+    end
 end
 function obj.toTable(self)
-	return {self.x,self.y,self.x+self.width,self.y+self.height}
+    return {self.x, self.y, self.x + self.width, self.y + self.height}
 end
 function obj.toNaiveRect(self)
-	if _G.type(naiveRect)~="userdata" then
-		Zlog.fatal("无法获取全局拓展Rect类,转换失败,这可能是运行引擎版本错误造成的,请尝试更换至2.0或更新版本的引擎再运行,或者开启低版本兼容模式")
-	end
-	return naiveRect(self.x,self.y,self.width,self.height)
+    if _G.type(naiveRect) ~= "userdata" then
+        Zlog.fatal(
+            "无法获取全局拓展Rect类,转换失败,这可能是运行引擎版本错误造成的,请尝试更换至2.0或更新版本的引擎再运行,或者开启低版本兼容模式")
+    end
+    return naiveRect(self.x, self.y, self.width, self.height)
 end
 
-
---类初始化函数,因为会循环引用,所以需要单独用函数初始化
+-- 类初始化函数,因为会循环引用,所以需要单独用函数初始化
 function obj._init()
-	if not api then api = require "Zlibs.class.api" end
-	obj.findSequence=api.findColor
-	obj.findMultiSequence=api.findColors
+    if not api then api = require "Zlibs.class.api" end
+    obj.findSequence = api.findColor
+    obj.findMultiSequence = api.findColors
 end
---成员函数结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---类初始化
---rawset(_G,obj.__tag,setmetatable({},obj))
-obj.ZERO=obj:__call(0, 0, 0, 0)
-return setmetatable({},obj)
---类初始化结束
---/////////////////////////////////////////
---/////////////////////////////////////////
+-- 成员函数结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 类初始化
+-- rawset(_G,obj.__tag,setmetatable({},obj))
+obj.ZERO = obj:__call(0, 0, 0, 0)
+return setmetatable({}, obj)
+-- 类初始化结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
--- xsp/src/Zlibs/class/Sequence.lua
+++ xsp/src/Zlibs/class/Sequence.lua
@@ -6,153 +6,170 @@ local Circle = require "Zlibs.class.Circle"
 local math = require "Zlibs.class.math"
 local type = require "Zlibs.tool.type"
 local string = require "Zlibs.class.string"
-local obj={}
-local funcValues={}
-local AllSequence={}
+local obj = {}
+local funcValues = {}
+local AllSequence = {}
 local api
---version 1:1.9引擎 2:2.0引擎
+-- version 1:1.9引擎 2:2.0引擎
 local version = 1
 if table.unpack then version = 2 end
---默认变量
-obj.__tag="Sequence"
-obj.name="default"
+-- 默认变量
+obj.__tag = "Sequence"
+obj.name = "default"
 -- 点组
-obj.point={}
-obj.str=""
+obj.point = {}
+obj.str = ""
 
-obj.distroyed=false
---默认变量结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---内部函数
+obj.distroyed = false
+-- 默认变量结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 内部函数
 local function serializePoint(self)
-	local points = self.point
-	local str = {}
-	for i = 1,#points do
-		str[i] = points[i]:toString()
-	end
-	self.str = table.concat(str,",")
-	return self.str
+    local points = self.point
+    local str = {}
+    for i = 1, #points do str[i] = points[i]:toString() end
+    self.str = table.concat(str, ",")
+    return self.str
 end
---内部函数结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---元表设置
-function obj:__newindex(k,v)
-	rawset(self,k,v)
-	Zlog.warn("不建议的操作:设置了[%s]对象中[%s]元素的值为[%s]",self.__tag,tostring(k),tostring(v))
+-- 内部函数结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 元表设置
+function obj:__newindex(k, v)
+    rawset(self, k, v)
+    Zlog.warn(
+        "不建议的操作:设置了[%s]对象中[%s]元素的值为[%s]",
+        self.__tag, tostring(k), tostring(v))
 end
 function obj:__index(k)
-	if type(funcValues[k])=="function" then return funcValues[k](self) end
-	if type(k)=="number" then return self.point[k] end
-	return obj[k]
+    if type(funcValues[k]) == "function" then return funcValues[k](self) end
+    if type(k) == "number" then return self.point[k] end
+    return obj[k]
 end
 function obj:__tostring()
-	return string.format("%s \"%s\" \"%s\"",obj.__tag,self.name,self.str)
+    return string.format("%s \"%s\" \"%s\"", obj.__tag, self.name, self.str)
 end
 function obj:__call(name)
-	if type(name)~="string" then
-		Zlog.fatal("请使用字符串作为序列的名称")
-	end
-	local o=setmetatable({name=name,point={},str=""}, obj)
-	if AllSequence[name] then
-		Zlog.warn("颜色序列类型出现重名[%s],旧数据将会被新数据覆盖",name)
-		o=AllSequence[name]
-	end
-	AllSequence[name] = o
-	return function (p)
-		return o:add(p)
-	end
+    if type(name) ~= "string" then
+        Zlog.fatal("请使用字符串作为序列的名称")
+    end
+    local o = setmetatable({name = name, point = {}, str = ""}, obj)
+    if AllSequence[name] then
+        Zlog.warn(
+            "颜色序列类型出现重名[%s],旧数据将会被新数据覆盖",
+            name)
+        o = AllSequence[name]
+    end
+    AllSequence[name] = o
+    return function(p) return o:add(p) end
 end
---元表设置结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---自动变量
+-- 元表设置结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 自动变量
 
---自动变量结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---成员函数
-function obj.get(name)
-	return AllSequence[name]
-end
+-- 自动变量结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 成员函数
+function obj.get(name) return AllSequence[name] end
 function obj.distroy(self)
-	if type(self)=="string" then
-		self=obj.get(self)
-	end
-	AllSequence[self.name]=nil
-	rawset(self,"distroyed",true)
+    if type(self) == "string" then self = obj.get(self) end
+    AllSequence[self.name] = nil
+    rawset(self, "distroyed", true)
 end
 function obj.toString(self)
-	serializePoint(self)
-	return self.str
+    serializePoint(self)
+    return self.str
 end
 function obj.del(self)
-	for k,v in ipairs(self.point) do
-		v:distroy()
-	end
-	self.point={}
-	serializePoint(self)
+    for k, v in ipairs(self.point) do v:distroy() end
+    self.point = {}
+    serializePoint(self)
 end
-function obj.add(self,p)
-	if type(p)=="string" then
-		p=string.split(p ,"," )
-	end
-	if type(p)~="table" then
-		Zlog.fatal("参数传入错误,请传入table或string格式的颜色序列")
-	end
-	local index=#self.point+1
-	for k,v in ipairs(p) do
-		local pos=Point("[AUTO]_" .. self.name .. "_" .. index)(v)
-		table.insert(self.point,pos)
-		index=#self.point+1
-	end
-	serializePoint(self)
-	return self
+function obj.add(self, p)
+    if type(p) == "string" then p = string.split(p, ",") end
+    if type(p) ~= "table" then
+        Zlog.fatal(
+            "参数传入错误,请传入table或string格式的颜色序列")
+    end
+    local index = #self.point + 1
+    for k, v in ipairs(p) do
+        local pos = Point("[AUTO]_" .. self.name .. "_" .. index)(v)
+        table.insert(self.point, pos)
+        index = #self.point + 1
+    end
+    serializePoint(self)
+    return self
 end
-function obj.match(self,fuzz)
-	for _,v in ipairs(self.point) do
-		if not v:match(fuzz) then return false end
-	end
-	return true
+function obj.match(self, fuzz)
+    for _, v in ipairs(self.point) do
+        if not v:match(fuzz) then return false end
+    end
+    return true
 end
-if version==1 then
-	obj.findInRect=function(self,rect,degree,hdir,vdir,priority)
-		return api.findColor(rect,self,degree,hdir,vdir,priority)
-	end
-	obj.multiFindInRect=function(self,rect,degree,hdir,vdir,priority,limit)
-		return api.findColors(rect,self,degree,hdir,vdir,priority,limit)
-	end
+if version == 1 then
+    obj.findInRect = function(self, rect, degree, hdir, vdir, priority)
+        return api.findColor(rect, self, degree, hdir, vdir, priority)
+    end
+    obj.multiFindInRect = function(self, rect, degree, hdir, vdir, priority,
+                                   limit)
+        return api.findColors(rect, self, degree, hdir, vdir, priority, limit)
+    end
+    obj.findInRange = function(self, range, degree)
+        return api.findColor(Rect(self.point[1] + Point(-range.x,-range.y), self.point[1] + range),
+                             self, degree)
+    end
+    obj.findInRangeTap = function(self, range, degree)
+        local p = api.findColor(Rect(self.point[1] + Point(-range.x,-range.y),
+                                     self.point[1] + range), self, degree)
+        if p ~= Point.INVALID then
+            p:tap()
+            return true
+        end
+        return false
+    end
 
-elseif version==2 then
-	obj.findInRect=function(self,rect,globalFuzz,priority)
-		return api.findColor(rect,self,globalFuzz,priority)
-	end
-	obj.multiFindInRect=function(self,rect,globalFuzz,priority,limit)
-		return api.findColors(rect,self,globalFuzz,priority,limit)
-	end
-end
-function obj.tap(self)
-	if self.point[1] then self.point[1]:tap() end
-end
+elseif version == 2 then
+    obj.findInRect = function(self, rect, globalFuzz, priority)
+        return api.findColor(rect, self, globalFuzz, priority)
+    end
+    obj.multiFindInRect = function(self, rect, globalFuzz, priority, limit)
+        return api.findColors(rect, self, globalFuzz, priority, limit)
+    end
+    obj.findInRange = function(self, range, globalFuzz)
+        return api.findColor(Rect(self.point[1] + Point(-range.x,-range.y), self.point[1] + range),
+                             self, globalFuzz)
+    end
+    obj.findInRangeTap = function(self, range, globalFuzz)
+        local p = api.findColor(Rect(self.point[1] + Point(-range.x,-range.y),
+                                     self.point[1] + range), self, globalFuzz)
+        if p ~= Point.INVALID then
+            p:tap()
+            return true
+        end
+        return false
 
-function obj.matchTap(self,fuzz)
-	if self:match(fuzz) then
-		if self.point[1] then self.point[1]:tap() end
-		return true
-	end
-	return false
+    end
 end
---类初始化函数,因为会循环引用,所以需要单独用函数初始化
-function obj._init()
-	if not api then api = require "Zlibs.class.api" end
+function obj.tap(self) if self.point[1] then self.point[1]:tap() end end
+
+function obj.matchTap(self, fuzz)
+    if self:match(fuzz) then
+        if self.point[1] then self.point[1]:tap() end
+        return true
+    end
+    return false
 end
---成员函数结束
---/////////////////////////////////////////
---/////////////////////////////////////////
---类初始化
---rawset(_G,obj.__tag,setmetatable({},obj))
-return setmetatable({},obj)
---类初始化结束
---/////////////////////////////////////////
---/////////////////////////////////////////
+-- 类初始化函数,因为会循环引用,所以需要单独用函数初始化
+function obj._init() if not api then api = require "Zlibs.class.api" end end
+-- 成员函数结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
+-- 类初始化
+-- rawset(_G,obj.__tag,setmetatable({},obj))
+return setmetatable({}, obj)
+-- 类初始化结束
+-- /////////////////////////////////////////
+-- /////////////////////////////////////////
